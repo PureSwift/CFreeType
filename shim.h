458072029a49cd7c943cc8e209381f9831a461ec
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-05-18 10:04:22 -0500

    fixed shim header

--- shim.h
+++ shim.h
@@ -1,4 +1,9 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#ifdef __linux__
+#include <freetype/freetype.h>
+#include <freetype/tttables.h>
+#else
 #include <freetype.h>
 #include <tttables.h>
+#endif
