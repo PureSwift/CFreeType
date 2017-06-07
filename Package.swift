5d65c8825addca5f30ff2c16e8b7e6f61562f618
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-07 02:04:21 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,10 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CFreeType",
     pkgConfig: "freetype2",
-    providers: [.Brew("freetype2"), .Apt("freetype2")]
+    providers: [.brew(["freetype2"]), .apt(["libfreetype2-dev"])]
 )
