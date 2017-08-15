f5eecb67e9225f1881813831e6e9beacfb440d50
Serhii Londar <serhii.londar@gmail.com>
2017-08-15 21:09:32 +0300

    Fixed import for Mac Os

--- shim.h
+++ shim.h
@@ -1,9 +1,3 @@
 #include <ft2build.h>
-#include FT_FREETYPE_H
-#ifdef __linux__
 #include <freetype/freetype.h>
 #include <freetype/tttables.h>
-#else
-#include <freetype.h>
-#include <tttables.h>
-#endif
