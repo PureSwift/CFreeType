079909dc68c13c700bb6bab3059ddf51f642b43a
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-22 02:38:06 -0500

    Fixed for Swift 3.0.2 compatibility

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:3.0.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,5 +6,5 @@ import PackageDescription
 let package = Package(
     name: "CFreeType",
     pkgConfig: "freetype2",
-    providers: [.brew(["freetype2"]), .apt(["libfreetype2-dev"])]
+    providers: [.Brew("freetype2"), .Apt("libfreetype6-dev")]
 )
