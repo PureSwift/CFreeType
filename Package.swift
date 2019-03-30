e93a10a49af7d8a0e922e2cebfb593fe64ce14d8
fwcd <fwcdmail@gmail.com>
2019-03-30 21:27:45 +0100

    Update Package.swift to Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
-// swift-tools-version:3.0.2
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CFreeType",
-    pkgConfig: "freetype2",
-    providers: [.Brew("freetype2"), .Apt("libfreetype6-dev")]
+    products: [
+        .library(name: "CFreeType", targets: ["CFreeType"])
+    ],
+    targets: [
+        .systemLibrary(
+            name: "CFreeType",
+            pkgConfig: "freetype2",
+            providers: [.brew(["freetype2"]), .apt(["libfreetype6-dev"])]
+        )
+    ]
 )
