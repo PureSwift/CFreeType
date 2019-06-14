95e1091b66147c04785a0912600dd74d05974528
Jakub Towarek <kuba@towarek.pl>
2019-06-15 00:01:22 +0200

    Fix paths to headers

--- shim.h
+++ shim.h
@@ -3,6 +3,9 @@
 #ifdef __linux__
 #include <freetype/freetype.h>
 #include <freetype/tttables.h>
+#elif defined(__APPLE__)
+#include <freetype/freetype.h>
+#include <freetype/tttables.h>
 #else
 #include <freetype.h>
 #include <tttables.h>
